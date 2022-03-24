38684582cfffb01b790b43756c525fc588748976
D1esel9 <67990404+D1esel9@users.noreply.github.com>
2022-03-24 11:58:03 +0400

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt install ubuntu-desktop -y --fix-missing
-wget https://download.nomachine.com/download/7.3/Linux/nomachine_7.3.2_1_amd64.deb
-sudo dpkg -i nomachine_7.3.2_1_amd64.deb
+wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb
+sudo dpkg -i nomachine_7.9.2_1_amd64.deb
 sudo apt-get install firefox -y
